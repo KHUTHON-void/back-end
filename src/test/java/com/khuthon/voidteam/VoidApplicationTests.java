8afa82ec38ece7f068846f658d1a0fa2dd0e4e63
Martin0o0 <8546088@naver.com>
2023-11-09 21:21:39 +0900

    init: 리파지토리 생성

---
+++ src/test/java/com/khuthon/voidteam/VoidApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.khuthon.voidteam;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class VoidApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
