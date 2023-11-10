d6e6260b14b5c0dd9aeff49fb5f1abec5e84aa98
Martin0o0 <8546088@naver.com>
2023-11-11 02:50:35 +0900

    에러 수정

--- src/main/java/com/khuthon/voidteam/controller/BoardController.java
+++ src/main/java/com/khuthon/voidteam/controller/BoardController.java
@@ -23,7 +23,7 @@ public class BoardController {
     private final BoardService boardService;
 
 
-    @GetMapping("rooms")
+    @GetMapping("/rooms")
     public ResponseEntity<?> getBoards(Principal principal,@RequestParam(value = "category", required = false) String category, @RequestParam(value = "sort") String sort){
         return boardService.getBoards(principal, category, sort);
     }
