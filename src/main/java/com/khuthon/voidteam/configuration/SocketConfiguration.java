103192841f9be7b692c2e68746a98f0dbe8f7350
Martin0o0 <8546088@naver.com>
2023-11-11 02:28:30 +0900

    추가 반영

--- src/main/java/com/khuthon/voidteam/configuration/SocketConfiguration.java
+++ src/main/java/com/khuthon/voidteam/configuration/SocketConfiguration.java
@@ -1,17 +1,24 @@
 package com.khuthon.voidteam.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class SocketConfiguration implements WebSocketConfigurer  {
+    @Autowired
+    private final WebSocketHandler chatHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketHandler(), "/socket").setAllowedOrigins("*");
+        registry.addHandler(chatHandler, "/socket").setAllowedOrigins("*");
     }
 }
