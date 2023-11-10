e2fc29087754d303c17ddff1212cad16c4aabd9e
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-10 21:57:11 +0900

    Recruit 추가

---
+++ src/main/java/com/khuthon/voidteam/domain/Recruit.java
@@ -0,0 +1,13 @@
+package com.khuthon.voidteam.domain;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Recruit {
+
+}
